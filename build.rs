df03228d547f4565b9ec7aa19fa8f86e33a5015b
akhercha <akherchache@pm.me>
2025-04-11 10:55:37 +0200

    🔨 Bump pragma-common to 0.1.13

--- build.rs
+++ build.rs
@@ -1,11 +1,13 @@
 fn main() {
-    // Notify Cargo to re-run this script if the schema file changes
-    println!("cargo:rerun-if-changed=capnp/schema.capnp");
+    if std::env::var("CARGO_FEATURE_CAPNP").is_ok() {
+        // Notify Cargo to re-run this script if the schema file changes
+        println!("cargo:rerun-if-changed=schema/schema.capnp");
 
-    // Compile the Cap'n Proto schema and output to OUT_DIR
-    capnpc::CompilerCommand::new()
-        .src_prefix("schema")
-        .file("schema/schema.capnp")
-        .run()
-        .expect("Failed to compile Cap'n Proto schema");
+        // Compile the Cap'n Proto schema and output to OUT_DIR
+        capnpc::CompilerCommand::new()
+            .src_prefix("schema")
+            .file("schema/schema.capnp")
+            .run()
+            .expect("Failed to compile Cap'n Proto schema");
+    }
 }
