03d1cf95c3b9af327e764b63e5282b126dea687b
0xevolve <Artevolve@yahoo.com>
2025-04-17 13:34:05 +0100

    fix: move starknet_network

--- src/starknet/mod.rs
+++ src/starknet/mod.rs
@@ -1,2 +1,7 @@
 pub mod errors;
+pub mod network;
 pub mod typed_data;
+
+pub use errors::*;
+pub use network::*;
+pub use typed_data::*;
--- src/web3/mod.rs
+++ src/web3/mod.rs
@@ -1,7 +1,5 @@
 pub mod chain;
-pub mod starknet_network;
 pub mod token;
 
 pub use chain::*;
-pub use starknet_network::*;
 pub use token::*;
