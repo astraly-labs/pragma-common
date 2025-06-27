9722e143de1120df83cdef1e068b73a38b5cd412
akhercha <akherchache@pm.me>
2025-06-27 18:03:40 +0200

    ⬆️ Bump version to 0.3.18

--- src/trading.rs
+++ src/trading.rs
@@ -1,13 +1,7 @@
-#[cfg(feature = "serde")]
-use std::str::FromStr;
-
-#[cfg(feature = "serde")]
-use serde::{Deserialize, Deserializer};
-
 #[derive(
     Clone, Debug, Hash, PartialEq, PartialOrd, Ord, Eq, Copy, strum::EnumString, strum::Display,
 )]
-#[cfg_attr(feature = "serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(
     feature = "borsh",
     derive(borsh::BorshSerialize, borsh::BorshDeserialize)
@@ -24,16 +18,5 @@ impl Side {
             Side::Long => Side::Short,
             Side::Short => Side::Long,
         }
-    }
-}
-
-#[cfg(feature = "serde")]
-impl<'de> Deserialize<'de> for Side {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        Side::from_str(&s).map_err(serde::de::Error::custom)
     }
 }
